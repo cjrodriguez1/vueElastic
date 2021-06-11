a7a96c63e06d4f8ae1dc4da54c204307b217f3be
Carlos Rodriguez <carlos.csci@gmail.com>
2021-06-11 14:09:25 -0400

    Added CSS for the form

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app" class="container">
 
- <div id="example-2">
-   <input type="text" v-model="id">
+ <div id="indexSection">
+  <spam style="font-weight:bold">Index:</spam> <input type="text"  v-model="index">
+</div>
+
+ <div id="idSection">
+  <spam style="font-weight:bold">ID:</spam> <input type="text"  v-model="id" required> 
   <button v-on:click="addMore">Search</button>
   
-  <p>Result: {{ callResult }} </p>
+  <p> {{ callResult }} </p>
 </div>
-<div>
-  <textarea v-model="message" ></textarea>
+<div id="resultsArea" style="display:none">
+  <div><spam style="font-weight:bold">Date:</spam> <input type="text"  v-model="date"></div>
+  <div> <spam style="font-weight:bold">Message:</spam> <input type="text"  v-model="message"></div>
+  <div><spam style="font-weight:bold">Level:</spam> <input type="text"  v-model="level"></div>
+  <div><spam style="font-weight:bold">Process:</spam> <input type="text"  v-model="process"></div>
+  <div><spam style="font-weight:bold">Direction:</spam> <input type="text"  v-model="direction"></div>
+  <div><spam style="font-weight:bold">Thread:</spam> <input type="text"  v-model="thread"></div>
+  <div><spam style="font-weight:bold">Event:</spam> <input type="text"  v-model="event"></div>
+ 
 </div>
   </div>
 </template>
@@ -20,31 +31,58 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      index: '',
       id:'',
       query: '',
       counter : 0,
       callResult : '',
-      message: 'Orale carnalito',
       element: '',
+      date: '',
+      level: '',
+      process: '',
+      direction: '',
+      thread: '',
+      event: '',
+      message: '',
     }
   },
   methods: {
     search() {
     },
     addMore() {
-      alert("Calling Elastic");
-      var url = 'http://localhost:9200/esta/' + this.id;
-      axios.get(url + this.element)
-    .then(response => {
-      // JSON responses are automatically parsed.
-      this.callResult = response.data;
-      console.log(this.callResult.esta.mappings.properties);
-      this.message = this.callResult.esta.mappings.properties;
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
-      
+      if(this.index.length == 0){
+        alert("Index Missing")
+      }
+      if(this.id.length == 0){
+        //alert("Please Enter a Valid Id Value");
+      }
+      else {
+            this.callResult = '';
+            var url = 'http://localhost:9200/' + this.index + '/_doc/' + this.id;
+            axios.get(url + this.element)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            this.callResult = response.data;
+            console.log(this.callResult);
+            this.date = this.callResult._source.DATE;
+            this.level = this.callResult._source.LEVEL;
+            this.process = this.callResult._source.PROCESS;
+            this.direction = this.callResult._source.DIRECTION;
+            this.thread = this.callResult._source.THREAD;
+            this.event = this.callResult._source.EVENT;
+            this.message = this.callResult._source.MESSAGE;
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      }
+      if(this.callResult.length == 0){
+        this.callResult = "No Results Found for ID: " + this.id;
+      }
+      document.getElementById("resultsArea").style.display = "block";
+      /*else {
+        document.getElementById("resultsArea").style.display = "block";
+      }*/
     },
   }
 }
@@ -59,47 +97,35 @@ body {
     margin-top: 50px;
     margin-left: 200px;
   }
-.card-row {
-  display: flex;
-  justify-content: center;
-  align-items: center;  
-  min-width: 780px;
+
+  input[type=text], select {
   width: 100%;
-  height: 500px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
 }
-.card {
-  position: relative;
-  background-color: #FFFFFF;
-  height: 370px;
-  width: 240px;
-  margin: 10px;
-  overflow: hidden;
-  box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5);
+
+button {
+  width: 25%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
-.card-image {
-  position: absolute;
-  left: -9999px;
-  right: -9999px;
-  margin: auto;
-  height: 220px;
-  min-width: 100%;
+
+button:hover {
+  background-color: #45a049;
 }
-.card-footer {
-  position: absolute;
-  bottom: 0;
-  height: 130px;
-  padding: 10px 15px;
-  font-family: Helvetica;
-}
-.card-text {
-  font-size: 14px;
-  color: rgba(0, 0, 0, 0.7);
-}
-.card-title {
-  font-family: Serif;
-}
-.card-author {
-  font-size: 14px;
-  color: #BAB096;
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
 }
 </style>
